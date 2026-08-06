9dd7163a932dc247575dbed4bfb4524de97f9fb3
Donrussell-21 <russellvizarra21@gmail.com>
2026-08-06 18:35:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,107 +3,14 @@ let html5QrCode = null;
 
 function startScanner() {
 
+    alert("Start Scanner button works!");
+
     document.getElementById("scanMessage").innerHTML =
-    "Opening camera...";
+        "Starting camera...";
 
+    html5QrCode = new Html5Qrcode("reader");
 
-    Html5Qrcode.getCameras()
-    .then(cameras => {
-
-
-        if (cameras && cameras.length) {
-
-
-            // Select first available laptop camera
-            let cameraId = cameras[0].id;
-
-
-            html5QrCode = new Html5Qrcode("reader");
-
-
-            html5QrCode.start(
-
-                cameraId,
-
-                {
-                    fps: 10,
-                    qrbox: {
-                        width: 250,
-                        height: 250
-                    }
-                },
-
-
-                qrCodeMessage => {
-
-
-                    console.log("QR Result:", qrCodeMessage);
-
-
-                    document.getElementById("scanMessage").innerHTML =
-
-                    `
-                    <div class="student-info">
-
-                    <p><strong>QR Code:</strong> ${qrCodeMessage}</p>
-
-                    </div>
-                    `;
-
-
-                    stopScanner();
-
-
-                    // Send QR ID to Google Script
-                    checkStudent(qrCodeMessage);
-
-
-                },
-
-
-                errorMessage => {
-
-                    // Ignore scanning errors
-
-                }
-
-
-            );
-
-
-        }
-
-        else {
-
-
-            document.getElementById("scanMessage").innerHTML =
-            "No camera detected";
-
-
-        }
-
-
-    })
-
-
-    .catch(err => {
-
-
-        console.error(err);
-
-
-        document.getElementById("scanMessage").innerHTML =
-
-        `
-        <div class="error">
-        Camera Error: ${err}
-        </div>
-        `;
-
-
-    });
-
-
+    ...
 }
 
 
